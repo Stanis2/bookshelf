84d2e71e632b80f904b85d49d9aaacafd40f7e6d
java_tsi08 <stanis2@inbox.lv>
2019-01-03 20:51:22 +0200

    03.01.19 - TSI

---
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/CreateBookBean.java
@@ -0,0 +1,42 @@
+package lv.tsi.javacourses.bookshelf.books.boundary;
+
+import lv.tsi.javacourses.bookshelf.books.model.BookEntity;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.io.Serializable;
+
+@ViewScoped
+@Named
+public class CreateBookBean implements Serializable {
+    @PersistenceContext
+    private EntityManager em;
+
+    private BookEntity book = new BookEntity();
+    private boolean created = false;
+
+    @Transactional
+    public String createBook() {
+        System.out.println("New book added");
+        em.persist(book);
+        book = new BookEntity();
+        created = true;
+        //return "book-created.xhtml";
+        return null;
+    }
+
+    public boolean isCreated() {
+        return created;
+    }
+
+    public BookEntity getBook() {
+        return book;
+    }
+
+    public void setBook(BookEntity book) {
+        this.book = book;
+    }
+}
--- src/main/java/lv/tsi/javacourses/bookshelf/books/model/BookEntity.java
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/model/BookEntity.java
@@ -1,8 +1,10 @@
 package lv.tsi.javacourses.bookshelf.books.model;
 
-import lv.tsi.javacourses.bookshelf.auth.model.Role;
-
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity(name = "Book")
@@ -11,14 +13,28 @@ public class BookEntity implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Size (min = 1, max = 200)
+    @NotEmpty (message = "Enter a valid book title.")
     @Column(name = "book_title", length = 200, nullable = false)
     private String title;
+
+    @Size (min = 1, max = 50)
+    @NotEmpty (message = "Enter a valid ISBN.")
     @Column(name = "book_isbn", length = 50, unique = true, nullable = false)
     private String isbn;
+
+    @Size (min = 1, max = 100)
+    @NotEmpty (message = "Enter a valid book author.")
     @Column(name = "book_author", length = 100, nullable = false)
     private String author;
+
+    @NotNull (message = "Enter a year between 1000 and this year.")
+    @Min(value = 1000)
     @Column(name = "book_year", nullable = false)
     private int year;
+
+    @Size (max = 2000, message = "No more than 2000 symbols.")
     @Column(name = "book_description", length = 2000)
     private String description;
 
